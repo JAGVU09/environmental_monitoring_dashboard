745610973454bace612d4a5079d293f4049cc808
James Gilbert <jamesgilbert@Jamess-MacBook-Air.local>
2022-06-08 14:24:57 -0500

    update code to use terra and raster because rgdal will be deprecated. sped up the load time by adding code to tell read_excel to import Site Locations as test. It was confused by the - and :

--- EM_Dashboard/global.R
+++ EM_Dashboard/global.R
@@ -14,38 +14,26 @@ library(lubridate)
 #Read in bulk manufacturing png
 bulk <- readPNG('./images/Bulk Manufacturing Area.png')
 
-bulk_blue <- raster(bulk[, , 1])
-bulk_green <- raster(bulk[, , 2])
-bulk_red <- raster(bulk[, , 3])
+bulk_img <- brick(bulk)
 
-bulk_img <- brick(bulk_red, bulk_green, bulk_blue)
-
-bulk_m <- viewRGB(bulk_img, maxpixels =  950716)
+bulk_m <- viewRGB(bulk_img, 1, 2, 3, maxpixels =  950716)
 
 #Read in aseptic air png
 aseptic_air <- readPNG('./images/Asceptic Fill Suite Air.png')
 
-aseptic_air_blue <- raster(aseptic_air[, , 1])
-aseptic_air_green <- raster(aseptic_air[, , 2])
-aseptic_air_red <- raster(aseptic_air[, , 3])
+aseptic_air_img <- brick(aseptic_air)
 
-aseptic_air_img <- brick(aseptic_air_red, aseptic_air_green, aseptic_air_blue)
-
-aseptic_air_m <- viewRGB(aseptic_air_img, maxpixels =  1117363)
+aseptic_air_m <- viewRGB(aseptic_air_img, 1, 2, 3, maxpixels =  1117363)
 
 #Read in aseptic surface
 aseptic_surf <- readPNG('./images/Asceptic Fill Suite Surface.png')
 
-aseptic_surf_blue <- raster(aseptic_surf[, , 1])
-aseptic_surf_green <- raster(aseptic_surf[, , 2])
-aseptic_surf_red <- raster(aseptic_surf[, , 3])
+aseptic_surf_img <- brick(aseptic_surf)
 
-aseptic_surf_img <- brick(aseptic_surf_red, aseptic_surf_green, aseptic_surf_blue)
-
-aseptic_surf_m <- viewRGB(aseptic_surf_img, maxpixels =  1117363)
+aseptic_surf_m <- viewRGB(aseptic_surf_img, 1, 2, 3, maxpixels =  1117363)
 
 #read in the data and clean it up for analysis
-micro<-read_excel('./data/eMIC Notebook - Berg.xlsx') %>% 
+micro<-read_excel('./data/eMIC Notebook - Berg.xlsx', col_types = c('Site_location' = 'text')) %>% 
   fill(`MID Number`, .direction = "down") %>% 
   fill(`Colony Morphology`, .direction = "down") %>% 
   fill(`Performance Date`, .direction = "down") %>% 
@@ -56,9 +44,22 @@ micro<-read_excel('./data/eMIC Notebook - Berg.xlsx') %>%
   mutate(Q = quarter(`date`, with_year = TRUE))
 
 #REGEX to clean up the sample sites
-micro$sample_site<-micro$sample_site %>%
-  str_replace('\\d+[:|-]\\s?\\d+:?\\s?:?[A-Za-z]+\\s?\\d+', '\\d+-\\d+') %>%
-  str_replace('-\\s+', '-')
+micro$sample_site<-micro$sample_site %>% 
+  str_trunc(.,6, ellipsis = '',side = c('right')) %>% 
+  str_replace('Air', '') %>% 
+  str_replace('-\\s+', '-') %>% 
+  str_squish %>% 
+  str_replace('-$', '') %>% 
+  str_replace('^[A-Za-z]+[\\s?|\\S?].[A-Za-z]*\\W\\d?\\W?|^0\\d+\\S\\w|\\d+mL|\\d% Cre', '') %>% 
+  str_replace('^[A-Za-z]+', '') %>%
+  str_replace('P|Run|[$]|10 mL|Dil|-Bac|-Fin|-che|-Che|\\d-Mi|-Sle|1-Lab|<62> T|-bac', '') %>% 
+  str_replace('^[-]', '') %>% 
+  str_replace(' ', '') %>% 
+  str_replace('108-\\d*\\w*|110-\\d*\\w*', "")
+
+micro$sample_site[micro$sample_site==""]<-NA
+
+micro<- micro %>% drop_na()
 
 #I think these actually need to go on the server side. 
 site_total_count<-micro %>% 
