3068819f0e3a511feaabd332bd967b068bc42c8c
James Gilbert <jamesgilbert@Jamess-MacBook-Air.local>
2022-06-10 15:48:10 -0500

    got some popup graphs working. working on some aesthetics

--- EM_Dashboard/global.R
+++ EM_Dashboard/global.R
@@ -11,6 +11,8 @@ library(ggplot2)
 library(tidyverse)
 library(lubridate)
 
+micro<- read_csv('./data/micro_em.csv')
+
 #Read in bulk manufacturing png
 bulk <- readPNG('./images/Bulk Manufacturing Area.png')
 
@@ -32,34 +34,7 @@ aseptic_surf_img <- brick(aseptic_surf)
 
 aseptic_surf_m <- viewRGB(aseptic_surf_img, 1, 2, 3, maxpixels =  1117363)
 
-#read in the data and clean it up for analysis
-micro<-read_excel('./data/eMIC Notebook - Berg.xlsx', col_types = c('Site_location' = 'text')) %>% 
-  fill(`MID Number`, .direction = "down") %>% 
-  fill(`Colony Morphology`, .direction = "down") %>% 
-  fill(`Performance Date`, .direction = "down") %>% 
-  pivot_longer(cols = starts_with('Site Location'), values_drop_na = TRUE) %>% 
-  dplyr::select(`Performance Date`, `MID Number`, `Colony Morphology`, value) %>% 
-  rename(date = `Performance Date`, ID = `MID Number`, sample_site = value) %>% 
-  mutate(date = parse_date_time(`date`,orders = 'd%B%Y%')) %>% 
-  mutate(Q = quarter(`date`, with_year = TRUE))
 
-#REGEX to clean up the sample sites
-micro$sample_site<-micro$sample_site %>% 
-  str_trunc(.,6, ellipsis = '',side = c('right')) %>% 
-  str_replace('Air', '') %>% 
-  str_replace('-\\s+', '-') %>% 
-  str_squish %>% 
-  str_replace('-$', '') %>% 
-  str_replace('^[A-Za-z]+[\\s?|\\S?].[A-Za-z]*\\W\\d?\\W?|^0\\d+\\S\\w|\\d+mL|\\d% Cre', '') %>% 
-  str_replace('^[A-Za-z]+', '') %>%
-  str_replace('P|Run|[$]|10 mL|Dil|-Bac|-Fin|-che|-Che|\\d-Mi|-Sle|1-Lab|<62> T|-bac', '') %>% 
-  str_replace('^[-]', '') %>% 
-  str_replace(' ', '') %>% 
-  str_replace('108-\\d*\\w*|110-\\d*\\w*', "")
-
-micro$sample_site[micro$sample_site==""]<-NA
-
-micro<- micro %>% drop_na()
 
 #I think these actually need to go on the server side. 
 site_total_count<-micro %>% 
@@ -68,10 +43,5 @@ site_total_count<-micro %>%
   ungroup() %>% 
   arrange(desc(tot_ids))
 
-site_q_count<-micro %>% 
-  group_by(sample_site, Q) %>% 
-  summarise(tot_ids = n()) %>% 
-  ungroup() %>% 
-  arrange(desc(tot_ids))
 
 sites<-micro$sample_site %>% unique()
--- EM_Dashboard/server.R
+++ EM_Dashboard/server.R
@@ -7,15 +7,17 @@ library(shinyWidgets)
 shinyServer(function(input, output, session) {
   output$mymap <- renderLeaflet({
     if (input$Facility == 'Aseptic Fill Suite - Air') {
-      aseptic_air_m@map
+      aseptic_air_m@map%>% 
+        addTiles() %>%
+        addCircles(lng = 0.08635, lat = 0.32595, radius = 2000, color = "#ff0033")
     }
     else if (input$Facility == 'Aseptic Fill Suite - Surface') {
       aseptic_surf_m@map
     }
     else {
-      bulk_m@map %>%
+      bulk_m@map %>% 
         addTiles(options = tileOptions(minZoom = 10, maxZoom = 11)) %>%
-        addCircles(lng = 0.17595, lat = 0.34418, radius = 1400)
+        addCircles(lng = lng, lat = lat, radius = 1400, color = "#ff0033")
     }
   })
 })
