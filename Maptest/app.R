4a54814a1a551a5d911777fa4dd7776e6b97d723
James Gilbert <jamesgilbert@Jamess-MacBook-Air.local>
2022-06-14 18:03:43 -0500

    I got the graphs working and they agree with the code from RMarkdown

--- Maptest/app.R
+++ Maptest/app.R
@@ -13,19 +13,34 @@ library(leafem)
 library(leafpop)
 library(shiny)
 library(plotly)
+library(shinythemes)
+#Read in the data
 micro <- read_csv('./micro_em.csv') %>%
   mutate(Week = week(date)) %>%
   group_by(sample_site, Week) %>%
   summarise(tot_ids = n()) %>%
   ungroup() %>%
-  arrange(desc(tot_ids))
+  arrange((Week))
+#create raster layer for the Bulk Manufacturing Area
 bulk <- readPNG('./Bulk Manufacturing Area.png')
 
 bulk_img <- brick(bulk)
 
 bulk_m <- viewRGB(bulk_img, 1, 2, 3, maxpixels =  950716)
+#create raster layer for the  aseptic air png
+aseptic_air <- readPNG('./Asceptic Fill Suite Air.png')
 
-myData <- data.frame(
+aseptic_air_img <- brick(aseptic_air)
+
+aseptic_air_m <- viewRGB(aseptic_air_img, 1, 2, 3, maxpixels =  1117363)
+
+#create raster layer for the aseptic surface
+aseptic_surf <- readPNG('./Asceptic Fill Suite Surface.png')
+
+aseptic_surf_img <- brick(aseptic_surf)
+
+aseptic_surf_m <- viewRGB(aseptic_surf_img, 1, 2, 3, maxpixels =  1117363)
+bulk_Data <- data.frame(
   lat = c(
     0.58364,
     0.58364,
@@ -81,43 +96,82 @@ myData <- data.frame(
     "315-3"
   )
 )
-ui <- fluidPage(leafletOutput("map"),
-                p(),
-                tableOutput('myTable'),
-                p(),
-                plotOutput("myPlot"),
-                p())
+ui <- fluidPage(theme = shinytheme('slate'),
+  titlePanel("Environmental Monitoring"),
+  sidebarLayout(sidebarPanel(
+    selectInput(
+      "Facility",
+      "Bulk Manufacturing Area & Aseptic Fill Suite",
+      choices = c(
+        'Bulk Manufacturing Area',
+        'Aseptic Fill Suite - Air',
+        'Aseptic Fill Suite - Surface'
+      )
+    )
+  ),
+  mainPanel(leafletOutput("map"),
+            p(),
+            plotOutput("myPlot"),
+            p(),
+            plotOutput('arima')
+            )
+  )
+  )
 server <- shinyServer(function(input, output) {
   data <- reactiveValues(clickedMarker = NULL)
   # produce the basic leaflet map with single marker
-  output$map <- renderLeaflet(
+  output$map <- renderLeaflet({
+    if (input$Facility == 'Aseptic Fill Suite - Air') {
+      aseptic_air_m@map %>%
+        addTiles(options = tileOptions(
+          minZoom = 0,
+          maxZoom = 18)) %>%
+        addCircles(
+          lng = 0.08635,
+          lat = 0.32595,
+          radius = 2000,
+          color = "#ff0033"
+        )
+    }
+    else if (input$Facility == 'Aseptic Fill Suite - Surface') {
+      aseptic_surf_m@map
+    }
+    else {
     bulk_m@map %>%
       addTiles() %>%
       addCircleMarkers(
-        lat = myData$lat,
-        lng = myData$lng,
-        layerId = myData$id
+        lat = bulk_Data$lat,
+        lng = bulk_Data$lng,
+        layerId = bulk_Data$id
       )
-  )
+      }
+  })
   # observe the marker click info and print to console when it is changed.
   observeEvent(input$map_marker_click, {
     print("observed map_marker_click")
     data$clickedMarker <- input$map_marker_click
     print(data$clickedMarker)
-    output$myTable <- renderTable({
-      return(subset(myData, id == data$clickedMarker$id))
-    })
     output$myPlot <- renderPlot({
+      #this str_detect inside the filter is not working properly. I need it to
+      # have the output from the click as a string + \\b but I can't figure it out
       micro %>%
         filter(str_detect(
-          sample_site, data$clickedMarker$id[[1]]
+          sample_site, paste0(data$clickedMarker$id[[1]],'\\b')
         )) %>% 
-        ggplot(aes(x = Week, y = tot_ids)) +
-        geom_line(show.legend = TRUE)+
+        ggplot(aes(x = Week, y = tot_ids, color = sample_site)) +
+        geom_line()+
         geom_hline(
           yintercept = c(75, 50),
           linetype = c('solid', 'dashed'),
-          color = c('red', 'orange'))
+          color = c('red', 'orange'))+
+        labs(x = "Week", y = "CFU", title ="Viable Count")
+    })
+    output$arima <- renderPlot({
+      #the graph is not outputting the same way it does in a RMarkdown
+      micro %>%
+        filter(str_detect(sample_site, paste0(data$clickedMarker$id[[1]],'\\b'))) %>%
+        select(tot_ids) %>% ts(frequency = 52)  %>%
+        auto.arima(lambda = 'auto') %>% forecast(h = 10) %>% plot()
     })
   })
 })
